330f23f112dbb6442e2f668333f950a56108a0a7
Dustin Willis Webber <dustin.webber@gmail.com>
2014-11-23 13:12:27 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# bindata 
+
+Package bindata is a helper module that allows to use in-memory static and template files for Macaron. (https://github.com/jteeuwen/go-bindata)
+
+Using go-bindata convert your template and public directories into individual packages. Import the
+packages and use them like the example below.
+
+## Example
+```go
+import "path/to/bindata/public"
+import "path/to/bindata/templates"
+
+m.Use(macaron.Static(
+  path.Join(setting.StaticRootPath, "public"),
+  macaron.StaticOptions{
+    SkipLogging: false,
+    FileSystem: bindata.Static(bindata.Options{
+      Asset:      public.Asset,
+      AssetDir:   public.AssetDir,
+      AssetNames: public.AssetNames,
+      Prefix:     "",
+    }),
+  },
+))
+
+m.Use(macaron.Renderer(macaron.RenderOptions{
+  Funcs:      []template.FuncMap{base.TemplateFuncs},
+  IndentJSON: macaron.Env != macaron.PROD,
+  TemplateFileSystem: bindata.Templates(bindata.Options{
+    Asset:      templates.Asset,
+    AssetDir:   templates.AssetDir,
+    AssetNames: templates.AssetNames,
+    Prefix:     "",
+  }),
+}))
+```
+
+# MIT LICENSE
+
+The MIT License (MIT) - [LICENSE](https://github.com/mephux/dnas/blob/master/LICENSE)
---
+++ bindata.go
@@ -0,0 +1,73 @@
+package bindata
+
+import (
+	"github.com/Unknwon/macaron"
+	"github.com/elazarl/go-bindata-assetfs"
+)
+
+type (
+	templateFileSystem struct {
+		files []macaron.TemplateFile
+	}
+
+	templateFile struct {
+		name string
+		data []byte
+	}
+
+	Options struct {
+		// Asset should return content of file in path if exists
+		Asset func(path string) ([]byte, error)
+		// AssetDir should return list of files in the path
+		AssetDir func(path string) ([]string, error)
+		// AssetNames should return list of all asset names
+		AssetNames func() []string
+		// Prefix would be prepended to http requests
+		Prefix string
+	}
+)
+
+func Static(opt Options) *assetfs.AssetFS {
+	fs := &assetfs.AssetFS{
+		Asset:    opt.Asset,
+		AssetDir: opt.AssetDir,
+		Prefix:   opt.Prefix,
+	}
+
+	return fs
+}
+
+func (templates templateFileSystem) ListFiles() []macaron.TemplateFile {
+	return templates.files
+}
+
+func (f *templateFile) Name() string {
+	return f.name
+}
+
+func (f *templateFile) Data() []byte {
+	return f.data
+}
+
+func Templates(opt Options) templateFileSystem {
+	fs := templateFileSystem{}
+	fs.files = make([]macaron.TemplateFile, 0, 10)
+
+	list := opt.AssetNames()
+
+	for _, key := range list {
+		ext := macaron.GetExt(key)
+
+		data, err := opt.Asset(key)
+
+		if err != nil {
+			continue
+		}
+
+		name := (key[0 : len(key)-len(ext)])
+
+		fs.files = append(fs.files, &templateFile{name, data})
+	}
+
+	return fs
+}
